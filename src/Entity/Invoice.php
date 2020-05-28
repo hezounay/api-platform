dfe7b20d39af83163d42ce95b2e8fc35ce8fe032
unknown <vanitashez@gmail.com>
2020-05-28 14:51:22 +0200

    api

---
+++ src/Entity/Invoice.php
@@ -0,0 +1,157 @@
+<?php
+
+namespace App\Entity;
+
+use App\Entity\User;
+use Doctrine\ORM\Mapping as ORM;
+use App\Repository\InvoiceRepository;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+
+/**
+ * @ORM\Entity(repositoryClass=InvoiceRepository::class)
+ * @ApiResource(
+ *  subresourceOperations={
+ *      "api_customers_invoices_get_subresource"={
+ *          "normalization_context"={"groups"={"invoices_subresource"}}
+ *      }
+ *  },
+ *  itemOperations={"GET","PUT","DELETE","PATCH","increment"= {
+ * 
+ *          "method"="post",
+ *          "path"="/invoices/{id}/increment",
+ *          "controller"="App\Controller\InvoiceIncrementationController",
+ *          "openapi_context"={
+ *              "summary"="Incrémente une facture",
+ *              "description"="Incrémente le chrono d'une facture donnée"
+ *          
+ * }
+ * 
+ * }},
+ *  attributes={
+ *      "pagination_enabled"=true,
+ *      "pagination_items_per_page"=20,
+ *      "order": {"sentAt":"desc"}
+ *  },
+ *  normalizationContext={"groups"={"invoices_read"}}
+ * )
+ * @ApiFilter(OrderFilter::class, properties={"amount","sentAt"})
+ */
+class Invoice
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     * @Groups({"invoices_read","customers_read","invoices_subresource"})
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     * @Groups({"invoices_read","customers_read"})
+     */
+    private $amount;
+
+    /**
+     * @ORM\Column(type="datetime")
+     * @Groups({"invoices_read","customers_read"})
+     */
+    private $sentAt;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Groups({"invoices_read","customers_read"})
+     */
+    private $status;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="invoices")
+     * @ORM\JoinColumn(nullable=false)
+     * @Groups({"invoices_read"})
+     */
+    private $customer;
+
+    /**
+     * @ORM\Column(type="integer")
+     * @Groups({"invoices_read","customers_read","invoices_subresource"})
+     */
+    private $chrono;
+
+    /**
+     * Permet de récupérer le User à qui appartient finalement la facture 
+     * @Groups({"invoices_read","invoices_subresource"})
+     *
+     * @return User
+     */
+    public function getUser(): User
+    {
+        return $this->customer->getUser();
+    }
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAmount(): ?float
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(float $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+
+    public function getSentAt(): ?\DateTimeInterface
+    {
+        return $this->sentAt;
+    }
+
+    public function setSentAt(\DateTimeInterface $sentAt): self
+    {
+        $this->sentAt = $sentAt;
+
+        return $this;
+    }
+
+    public function getStatus(): ?string
+    {
+        return $this->status;
+    }
+
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    public function getCustomer(): ?Customer
+    {
+        return $this->customer;
+    }
+
+    public function setCustomer(?Customer $customer): self
+    {
+        $this->customer = $customer;
+
+        return $this;
+    }
+
+    public function getChrono(): ?int
+    {
+        return $this->chrono;
+    }
+
+    public function setChrono(int $chrono): self
+    {
+        $this->chrono = $chrono;
+
+        return $this;
+    }
+}
